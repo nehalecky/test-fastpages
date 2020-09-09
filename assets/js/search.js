62133af4b4c10a58fdca6e306cb2cfa82acfe073
nehalecky <nehalecky@users.noreply.github.com>
2020-09-09 22:37:59 +0000

    deploy: b5dea55fa6ea3c981df115cbc6f8ca7ea6233c82

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/test-fastpages/assets/js/search-data.json', true);
+    request.open('GET', '/empiric-mind/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
